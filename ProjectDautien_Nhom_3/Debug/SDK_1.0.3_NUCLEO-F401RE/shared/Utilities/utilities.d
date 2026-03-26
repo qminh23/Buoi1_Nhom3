6065614e9abce3d609e452200ecdb1f1d0159af5
qminh23 <qminhh2310@gmail.com>
2026-03-26 12:31:01 +0700

    Dọn dẹp rác hệ thống DS_Store

--- ProjectDautien_Nhom_3/Debug/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.d
+++
@@ -1,4 +0,0 @@
-SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.o: \
- /Users/macos/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.c \
- /Users/macos/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.h
-/Users/macos/Downloads/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities/utilities.h:
